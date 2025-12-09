2413c39560fb88b24c0183a2f10e6f2ab13cb708
LuisArias <luisariasllaguno@gmail.com>
2025-12-09 17:55:58 -0600

    Commit Inicial
    Se crearon los archivos en donde se va a trabajar el proyecto. En el archivo de project.py es donde va a estar las versiones finales de los modelos dependiendo de los problemas. En el archivo de tests.ipynb se crearon las pruebas para los modelos y que se pueda manejar en un ambiente controlado

---
+++ project.py
@@ -0,0 +1,17 @@
+# Luis Alberto Arias Llaguno
+# David Ceballos Mata
+# Ruben
+
+"""
+En este archivo se encuentra el proyecto final del curso de Analítica Avanzada en un solo archivo de 
+Python en donde contiene todos los problemas que se plantearon resueltos con los mejores modelos y 
+los parámetros optimizados. El objetivo de este proyecto es resolver problemas de la NBA con modelos 
+de machine learning y deep learning.
+"""
+
+# Importación de librerías
+
+
+
+
+
